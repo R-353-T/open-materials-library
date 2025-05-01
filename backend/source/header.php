2cc261f3a141bcdc8d4c0c2f48e75760a6d44be3
Adrien Villalonga <nd4yza9cgmdx4cxpq4box6tv7nh80zgc@proton.me>
2025-05-01 19:04:53 +0200

    backend:feature: empty wordpress structure

---
+++ backend/source/header.php
@@ -0,0 +1,30 @@
+<?php
+
+$header_infos = array(
+    "charset"           => get_bloginfo("charset"),
+    "description"       => get_bloginfo("description"),
+    "body_classes"      => join(" ", get_body_class()),
+    "lang_attributes"   => get_language_attributes()
+);
+
+?>
+
+<!DOCTYPE html>
+<html lang="<?php echo $header_infos["lang_attributes"]; ?>">
+<head>
+    <!-- meta -->
+    <meta charset="<?php echo $header_infos["charset"]; ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?php echo $header_infos["description"]; ?>">
+
+    <!-- link -->
+    <!-- <link rel="icon" type="image/png" href=""> -->
+    
+    <!-- wp_head -->
+    <?php wp_head(); ?>
+</head>
+<body
+    class="<?php echo $header_infos["body_classes"]; ?>"
+    style="background: #000000; min-height: 100vh;">
+    <!-- wp_body_open -->
+    <?php wp_body_open(); ?>
