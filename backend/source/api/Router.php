4637f6ecc4bb53aea8f75762b19130ff64c49f11
Adrien Villalonga <nd4yza9cgmdx4cxpq4box6tv7nh80zgc@proton.me>
2025-05-14 20:44:09 +0200

    backend:feature: router

---
+++ backend/source/api/Router.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace oml\api;
+
+use oml\api\controller\MediaController;
+use oml\api\schema\MediaSchema;
+use oml\php\abstract\Service;
+use oml\php\enum\ControllerHttpMethod;
+use oml\php\enum\ControllerPermission;
+
+class Router extends Service
+{
+    private function getRoutes()
+    {
+        return [
+            // * MEDIA *
+
+            "media" => [
+                [
+                    "controller" => MediaController::inject(),
+                    "schema" => MediaSchema::inject(),
+                    "endpoints" => [
+                        [
+                            "callback"      => "create",
+                            "http_method"   => ControllerHttpMethod::POST,
+                            "permission"    => ControllerPermission::EDITOR
+                        ],
+                        [
+                            "callback"      => "get",
+                            "http_method"   => ControllerHttpMethod::GET,
+                            "permission"    => ControllerPermission::SUBSCRIBER,
+                        ],
+                        [
+                            "callback"      => "delete",
+                            "http_method"   => ControllerHttpMethod::DELETE,
+                            "permission"    => ControllerPermission::EDITOR,
+                        ],
+                        [
+                            "endpoint"      => "/list",
+                            "callback"      => "list",
+                            "http_method"   => ControllerHttpMethod::GET,
+                            "permission"    => ControllerPermission::SUBSCRIBER
+                        ],
+                        [
+                            "endpoint"      => "/update",
+                            "callback"      => "update",
+                            "http_method"   => ControllerHttpMethod::POST,
+                            "permission"    => ControllerPermission::EDITOR
+                        ]
+                    ]
+                ]
+            ]
+        ];
+    }
+
+    public function loadRoutes()
+    {
+        $routes = $this->getRoutes();
+
+        foreach ($routes as $routeNamespace => $routeOptions) {
+            $routeEndpoints = $routeOptions["endpoints"];
+            $routeController = $routeOptions["controller"];
+            $routeSchema = $routeOptions["schema"];
+
+            foreach ($routeEndpoints as $endpoint) {
+                $endpointUrl = isset($endpoint["endpoint"]) ? $endpoint["endpoint"] : "";
+
+                register_rest_route(
+                    OML_NAMESPACE,
+                    "/{$routeNamespace}{$endpointUrl}",
+                    [
+                        "callback"              => [$routeController, $endpoint["callback"]],
+                        "methods"               => $endpoint["http_method"],
+                        "permission_callback"   => $endpoint["permission"]
+                    ]
+                );
+
+                register_rest_route(
+                    OML_NAMESPACE,
+                    "/{$routeNamespace}{$endpointUrl}/schema",
+                    [
+                        "callback"              => [$routeSchema, $endpoint["callback"]],
+                        "methods"               => $endpoint["http_method"],
+                        "permission_callback"   => $endpoint["permission"]
+                    ]
+                );
+            }
+        }
+    }
+}
--- backend/source/functions.php
+++ backend/source/functions.php
@@ -3,30 +3,16 @@
 require_once __DIR__ . "/php/oml_constant.php";
 require_once __DIR__ . "/php/function/__index__.php";
 
-use oml\api\controller\EnumeratorController;
-use oml\api\controller\MediaController;
-use oml\api\controller\QuantityController;
-use oml\api\controller\TestController;
-use oml\api\controller\TypeController;
 use oml\api\middleware\AuthLimitMiddleware;
 use oml\api\middleware\BucketMiddleware;
-use oml\php\abstract\Controller;
+use oml\api\Router;
 use oml\php\abstract\Middleware;
 use oml\php\core\Database;
 
 # Database
 
 Database::initializeDatabase();
-
-# Controllers
-
-Controller::$controllerList = [
-    EnumeratorController::class,
-    TypeController::class,
-    MediaController::class,
-    QuantityController::class,
-    TestController::class
-];
+$router = Router::inject();
 
 # Middlewares
 
@@ -37,14 +23,14 @@ Middleware::$middlewareList = [
 
 # Filters
 
-add_filter("rest_exposed_cors_headers", "oml_expose_cors_headers_filter", 10, 2);
-add_filter("rest_endpoints", "oml_wp_block_original_endpoints_filter", 10, 1);
 add_filter("jwt_auth_expire", "oml_jwt_expiration_time_filter", 10, 0);
+add_filter("rest_endpoints", "oml_wp_block_original_endpoints_filter", 10, 1);
+add_filter("rest_exposed_cors_headers", "oml_expose_cors_headers_filter", 10, 2);
 
-add_filter("rest_pre_dispatch", [ Middleware::class, "requestFilter" ], 10, 3);
 add_filter("rest_post_dispatch", [ Middleware::class, "responseFilter" ], 10, 3);
+add_filter("rest_pre_dispatch", [ Middleware::class, "requestFilter" ], 10, 3);
 
 # Hooks
 
-add_action("rest_api_init", [ Controller::class, "loadControllers" ]);
 add_action("after_switch_theme", [ Database::class, "upgradeDatabase" ]);
+add_action("rest_api_init", [ $router, "loadRoutes" ]);
